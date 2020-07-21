5e1b343cbda898dc82e1b77c432f9680e639ef89
Mariola Pasko <paskomariola@gmail.com>
2020-07-21 17:50:27 +0100

    js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// UI Vars
+// Define UI Vars
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
@@ -10,31 +10,31 @@ loadEventListeners();
 
 // Load all event listeners
 function loadEventListeners() {
-  // Add a task event
+  // Add task event
   form.addEventListener('submit', addTask);
   // Remove task event
-  taskList.addEventListener('click', removeTask)
-  // Clear task event 
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
   clearBtn.addEventListener('click', clearTasks);
-  //Filter tasks event
-  filter.addEventListener('keyup', filterTask);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
 function addTask(e) {
-  if(taskInput.value === '') {
+  if (taskInput.value === '') {
     alert('Add a task');
   }
 
   // Create li element
   const li = document.createElement('li');
-  // Add a class
+  // Add class
   li.className = 'collection-item';
   // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
   // Create new link element
   const link = document.createElement('a');
-  // Add a class
+  // Add class
   link.className = 'delete-item secondary-content';
   // Add icon html
   link.innerHTML = '<i class="fa fa-remove"></i>';
@@ -50,7 +50,7 @@ function addTask(e) {
   e.preventDefault();
 }
 
-//Remove Task
+// Remove Task
 function removeTask(e) {
   if (e.target.parentElement.classList.contains('delete-item')) {
     if (confirm('Are You Sure?')) {
@@ -71,10 +71,11 @@ function clearTasks() {
   // https://jsperf.com/innerhtml-vs-removechild
 }
 
-//Filter Tasks
-function filterTask(e) {
+// Filter Tasks
+function filterTasks(e) {
   const text = e.target.value.toLowerCase();
-  document.querySelectorAll('.collection-item').forEach(function (taks) {
+
+  document.querySelectorAll('.collection-item').forEach(function (task) {
     const item = task.firstChild.textContent;
     if (item.toLowerCase().indexOf(text) != -1) {
       task.style.display = 'block';
