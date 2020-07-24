efb712dd587be75464bbca1ecfed0fe1a664da2f
Mariola Pasko <paskomariola@gmail.com>
2020-07-24 22:15:04 +0100

    js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function removeTask(e) {
 function clearTasks() {
   // taskList.innerHTML = '';
 
-  // Faster way
+  // Faster 
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
